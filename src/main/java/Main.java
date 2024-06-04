21001c3d96b7ab79d5d5d354ec313605ff149f92
Mohit Singh <xyzmohit.singh@gmail.com>
2024-06-05 00:01:24 +0530

    decode arg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ public class Main {
   private static final Gson gson = new Gson();
 
   public static void main(String[] args) throws Exception {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
     String command = args[0];
     if("decode".equals(command)) {
         String bencodedValue = args[1];
