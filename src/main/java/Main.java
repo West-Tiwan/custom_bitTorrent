7726832c5ee042a59a18166b0ae88f6003ff1c51
Mohit Singh <xyzmohit.singh@gmail.com>
2024-06-05 09:53:59 +0530

    int 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,25 @@ public class Main {
     String command = args[0];
     if("decode".equals(command)) {
         String bencodedValue = args[1];
-        String decoded;
+        Object decoded;
         try {
           decoded = decodeBencode(bencodedValue);
         } catch(RuntimeException e) {
           System.out.println(e.getMessage());
           return;
         }
-        System.out.println(gson.toJson(decoded));
-
+        if (Character.isDigit(bencodedValue.charAt(0))) {
+          System.out.println(gson.toJson(decoded));
+        } else {
+          System.out.println(gson.toJson(decoded).replaceAll("\"", ""));
+        }
     } else {
       System.out.println("Unknown command: " + command);
     }
 
   }
 
-  static String decodeBencode(String bencodedString) {
+  static Object decodeBencode(String bencodedString) {
     if (Character.isDigit(bencodedString.charAt(0))) {
       int firstColonIndex = 0;
       for(int i = 0; i < bencodedString.length(); i++) { 
@@ -34,8 +37,12 @@ public class Main {
       }
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
       return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
+    } else if (bencodedString.startsWith("i")) {
+      return Long.parseLong(
+              bencodedString.substring(1, bencodedString.indexOf("e")));
     } else {
-      throw new RuntimeException("Only strings are supported at the moment");
+      String value = bencodedString.replace("i", "").replace("e", "");
+      return value;
     }
   }
   
