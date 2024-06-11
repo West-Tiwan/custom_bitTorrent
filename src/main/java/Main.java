332d223f289e4ba0c940154731566da2756e29e1
Mohit Singh <xyzmohit.singh@gmail.com>
2024-06-11 15:07:53 +0530

    infohash-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,76 +1,52 @@
-import com.dampcake.bencode.Bencode; // - available if you need it!
-import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
-
-import java.math.BigInteger;
-import java.util.Map;
+import com.dampcake.bencode.Bencode;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.*;
+import java.util.Map;
 public class Main {
     private static final Gson gson = new Gson();
     public static void main(String[] args) throws Exception {
         String command = args[0];
         if ("decode".equals(command)) {
             String bencodedValue = args[1];
-            String decoded;
+            Object decoded;
             try {
-                decoded = decodeBencode(bencodedValue);
+                decoded = Bencode.decode(bencodedValue);
             } catch (RuntimeException e) {
                 System.out.println(e.getMessage());
                 return;
             }
-            System.out.println(decoded);
+            System.out.println(gson.toJson(decoded));
         } else if ("info".equals(command)) {
-            String torrentPath = args[1];
-            Torrent torrent = new Torrent(Files.readAllBytes(Path.of(torrentPath)));
-            System.out.println("Tracker URL: "+torrent.announce);
-            System.out.println("Length: "+torrent.length);
+            Path filePath = Path.of(args[1]);
+            if (!Files.exists(filePath) || !Files.isRegularFile(filePath)) {
+                throw new RuntimeException("not a file.");
+            }
+            var fileBytes = Files.readAllBytes(filePath);
+            Object decoded = Bencode.decode(fileBytes);
+            switch (decoded) {
+                case Map<?, ?> m -> {
+                    assert m.containsKey("announce") && m.containsKey("info");
+                    var trackerUrl = (String) m.get("announce");
+                    var infoDict = (Map<?, ?>) m.get("info");
+                    var length = (Long) infoDict.get("length");
+                    var infoHash = calculateHash(infoDict);
+                    System.out.printf("Tracker URL: %s\n", trackerUrl);
+                    System.out.printf("Length: %d\n", length);
+                    System.out.printf("Info Hash: %s\n", toHexString(infoHash));
+                    System.out.printf("Piece Length: %d\n", (long)infoDict.get("piece length"));
+                    printPieceHashes(infoDict);
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + decoded);
+            }
+            System.out.println(gson.toJson(decoded));
         } else {
             System.out.println("Unknown command: " + command);
         }
     }
-
-    static class Torrent {
-        public String announce;
-        public long length;
-        public Torrent(byte[] bytes) throws NoSuchAlgorithmException {
-            Bencode bencode = new Bencode(false);
-            Bencode bencode2 = new Bencode(true);
-            Map<String ,Object> root = bencode.decode(bytes,Type.DICTIONARY);
-            Map<String ,Object> info = (Map<String,Object>) root.get("info");
-            announce = (String) root.get("announce");
-            length = (Long) info.get("length");
-            var infoHash = calculateHash(info);
-            System.out.printf("Info Hash: %s\n", toHexString(infoHash));
-            System.out.printf("Piece Length: %d\n", (long)info.get("piece length"));
-            printPieceHashes(info);
-        }
-    }
-
-    private static String toHexString(byte[] data) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : data) {
-            sb.append(String.format("%02x", b));
-        }
-        return sb.toString();
-    }
-
-    private static byte[] calculateHash(Map<?, ?> infoDict) {
-        var stringEncoded = Bencode.encodeToByteBuff(infoDict);
-        try {
-            var sha1Digest = MessageDigest.getInstance("SHA-1");
-            sha1Digest.update(stringEncoded);
-            return sha1Digest.digest();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
-
     private static void printPieceHashes(Map<?,?> infoDict) {
         var data = (String)infoDict.get("pieces");
         var bytes = data.getBytes(StandardCharsets.ISO_8859_1);
@@ -82,39 +58,21 @@ public class Main {
             System.out.printf("%02x", bytes[i]);
         }
     }
-
-    static String decodeBencode(String bencodedString) {
-        Bencode bencode = new Bencode();
-        char firstChar = bencodedString.charAt(0);
-        Object decoded;
-        if (Character.isDigit(firstChar)) {
-            // bencoded string
-            int firstColonIndex = 0;
-            for (int i = 0; i < bencodedString.length(); i++) {
-                if (bencodedString.charAt(i) == ':') {
-                    firstColonIndex = i;
-                    break;
-                }
-            }
-            int length =
-                    Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-            decoded = bencodedString.substring(firstColonIndex + 1,
-                    firstColonIndex + 1 + length);
-        } else if (firstChar == 'i') {
-            // bencoded number
-            decoded = bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8),
-                    Type.NUMBER);
-        } else if (firstChar == 'l') {
-            // bencoded list
-            decoded = bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8),
-                    Type.LIST);
-        } else if (firstChar == 'd') {
-            // bencoded dictionary
-            decoded = bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8),
-                    Type.DICTIONARY);
-        } else {
-            throw new RuntimeException("Not supported");
+    private static String toHexString(byte[] data) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : data) {
+            sb.append(String.format("%02x", b));
         }
-        return gson.toJson(decoded);
+        return sb.toString();
+    }
+    private static byte[] calculateHash(Map<?, ?> infoDict) {
+        var stringEncoded = Bencode.encodeToByteBuff(infoDict);
+        try {
+            var sha1Digest = MessageDigest.getInstance("SHA-1");
+            sha1Digest.update(stringEncoded);
+            return sha1Digest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
